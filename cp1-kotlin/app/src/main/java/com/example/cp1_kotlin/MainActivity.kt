a1f63569b12a45da20d140fde0b05112f1aa00d1
lu <luisa.souzasantos42@gmail.com>
2026-03-24 22:45:34 -0300

    projeto incompleto
    corrigir erros e read me

---
+++ cp1-kotlin/app/src/main/java/com/example/cp1_kotlin/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.cp1_kotlin
+
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.cp1_kotlin.screens.LoginScreen
+import com.example.cp1_kotlin.screens.MenuScreen
+import com.example.cp1_kotlin.screens.PedidosScreen
+import com.example.cp1_kotlin.screens.PerfilScreen
+import com.example.cp1_kotlin.ui.theme.Android5navigationbetweenscreensTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Android5navigationbetweenscreensTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login",
+                    ) {
+                        composable(route = "login") {
+                            LoginScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "menu") {
+                            MenuScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(
+                            route = "pedidos?cliente={cliente}",
+                            arguments = listOf(navArgument("cliente") {
+                                defaultValue = "Cliente Genérico"
+                            })
+                        ) {
+                            PedidosScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                navController,
+                                it.arguments?.getString("cliente")
+                            )
+                        }
+                        composable(route = "perfil/{nome}") {
+                            val nome: String? = it.arguments?.getString("nome", "Usuário Genérico")
+                            PerfilScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                navController,
+                                nome!!
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
